6a9e449b04132edd47ad4444890c231bed7fa046
Simon the Noob <zhang.simonz@gmail.com>
2016-08-21 23:56:03 +1200

    displaying order details and calculations for the order total

--- scripts/main.js
+++ scripts/main.js
@@ -18,8 +18,12 @@ var App = React.createClass({
 		return {
 			fishes : {},
 			order : {}
-		};
-		
+		}
+	},
+	
+	addToOrder : function(key) {
+		this.state.order[key] = this.state.order[key] + 1 || 1;
+		this.setState({ order : this.state.order });
 	},
 	
 	addFish : function (fish) {
@@ -37,7 +41,7 @@ var App = React.createClass({
 	},
 	
 	renderFish : function (key) {
-		return <Fish key={key} index={key} details={this.state.fishes[key]} />
+		return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
 	},
 	
 	render : function () {
@@ -49,7 +53,7 @@ var App = React.createClass({
 						{Object.keys(this.state.fishes).map(this.renderFish)}
 					</ul>
 				</div>
-				<Order/>
+				<Order fishes={this.state.fishes} order={this.state.order}/>
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		);
@@ -63,8 +67,16 @@ var App = React.createClass({
  */
 var Fish = React.createClass({
 	
+	onButtonClick : function () {
+		console.log("Going to add the fish: ", this.props.index);
+		var key = this.props.index;
+		this.props.addToOrder(key);
+	},
+	
 	render : function () {
 		var details = this.props.details;
+		var isAvailable = (details.status === 'available' ? true : false);
+		var buttonText = (isAvailable ? 'Add To Order' : 'Sold Out!');
 		return (
 			<li className="menu-fish">
 				<img src={details.image} alt={details.name} />
@@ -73,6 +85,7 @@ var Fish = React.createClass({
 					<span className="price">{Helper.formatPrice(details.price)}</span>
 				</h3>
 				<p>{details.desc}</p>
+				<button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
 			</li>
 		)
 	}
@@ -147,10 +160,53 @@ var Header = React.createClass({
 	<Order />
  */
 var Order = React.createClass({
+	
+	renderOrder : function (key) {
+		var fish = this.props.fishes[key];
+		var count = this.props.order[key];
+		
+		if(!fish){
+			return <li key={key}>Sorry, fish no longer available!</li>
+		}
+		
+		// displays the details which are titles and amount in cart
+		return (
+			<li>
+				{count}lbs {fish.name}
+				<span className="price">
+					{Helper.formatPrice(count * fish.price)}
+				</span>
+			</li>
+		);
+	},
 
 	render : function () {
+		var orderIDs = Object.keys(this.props.order);
+		
+		var total = orderIDs.reduce((prevTotal, key) =>{
+			var fish = this.props.fishes[key];
+			var count = this.props.order[key];
+			// not calculating if it is not available
+			var isAvailable = fish && fish.status === 'available';
+			
+			if(fish && isAvailable){
+				return prevTotal + (count * parseInt(fish.price) || 0);
+			}
+			
+			return prevTotal;
+		}, 0);
+		
 		return (
-			<p>Order</p>
+			<div className="order-wrap">
+				<h2 className="order-title">Your Order</h2>
+				<ul className="order">
+					{orderIDs.map(this.renderOrder)}
+					<li className="total">
+						<strong>Total:</strong>
+						{Helper.formatPrice(total)}
+					</li>
+				</ul>
+			</div>
 		);
 	}
 
